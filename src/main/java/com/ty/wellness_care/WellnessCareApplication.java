86e6cd3715b38a25e34984c09a1e0406fa9d2c9c
pradeepapal <pradeepapalanichamybsc@gmail.com>
2022-02-17 15:01:48 -0800

    Initial Commit

---
+++ src/main/java/com/ty/wellness_care/WellnessCareApplication.java
@@ -0,0 +1,13 @@
+package com.ty.wellness_care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellnessCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellnessCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ty/wellness_care/WellnessCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.wellness_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellnessCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
